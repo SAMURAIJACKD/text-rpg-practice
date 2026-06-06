78bd35fc0789a7faf7b55be134c5dc2879abf421
SAMURAIJACKD <106445386+SAMURAIJACKD@users.noreply.github.com>
2026-06-06 01:44:15 -0500

    feat: added functionality for 2 buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,18 @@ import { useState } from 'react'
       const playerName = 'Ricky'
       const playerLevel = 1
       const playerHealth = 20
+      const playerBackpack = ['flashlight', 'rope', 'prybar']
       const [location, setLocation] = useState('Home')
       const [storyText, setStoryText] = useState(`You open your eyes on a couch, looking around you see your home. You don't remember how you got back home, but as you look at your table you see a note. It reads: "${playerName}, you need to complete the demo, then you can join us in the real game. We'll be waiting for you. - The Devs"`)
       function goToCity() {
         setLocation('City')
         setStoryText('You head into the city to see what you can find. As you walk to the Boulevard you see people bustling about. There are shops, some groups calling out to passersby, and vehicles carrying their passengers. What do you want to do?')
+      }
+      function checkBackpack() {
+        setStoryText(`You check your backpack, inside you have: ${playerBackpack.join(', ')}`)
+      }
+      function roadsideRest() {
+        setStoryText(`You set up a bedroll by the side of the road and rest for a while. You feel refreshed and have ${playerHealth} health`)
       }
       return (
         <main>
@@ -36,8 +43,8 @@ import { useState } from 'react'
             <h2>Choices</h2>
             <button onClick={goToCity}>Venture into the city</button>
               
-            <button>Check your backpack</button>
-            <button>Rest by the road</button>
+            <button onClick={checkBackpack}>Check your backpack</button>
+            <button onClick={roadsideRest}>Rest by the road</button>
           </section>
         </main>
       )
