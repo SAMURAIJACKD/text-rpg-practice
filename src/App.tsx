9c4a22995c7ffca97509d5bd5b55077b12a676f4
SAMURAIJACKD <106445386+SAMURAIJACKD@users.noreply.github.com>
2026-06-07 01:41:23 -0500

    feat: added rest function and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
 import { useState } from 'react'
-    
+import './App.css'
+
+
     function App() {
       const playerName = 'Ricky'
       const playerLevel = 1
-      const playerHealth = 20
+      const [playerMaxHealth, setplayerMaxHealth] = useState(20)
+      const [playerHealth, setplayerHealth] = useState(playerMaxHealth)
       const playerBackpack = ['flashlight', 'rope', 'prybar']
       const [location, setLocation] = useState('Home')
       const [storyText, setStoryText] = useState(`You open your eyes on a couch, looking around you see your home. You don't remember how you got back home, but as you look at your table you see a note. It reads: "${playerName}, you need to complete the demo, then you can join us in the real game. We'll be waiting for you. - The Devs"`)
@@ -16,7 +18,20 @@ import { useState } from 'react'
         setStoryText(`You check your backpack, inside you have: ${playerBackpack.join(', ')}`)
       }
       function roadsideRest() {
-        setStoryText(`You set up a bedroll by the side of the road and rest for a while. You feel refreshed and have ${playerHealth} health`)
+        if (playerHealth >= playerMaxHealth) {
+          setStoryText('You consider resting, but realize that you aren\'t actually tired. You decide to move on instead. \(Your health is already full, don\t be silly\).')
+          return
+        }
+        else {
+          const heal = Math.round(Math.random() * (playerMaxHealth - playerHealth))
+          const newHealth = Math.min(playerMaxHealth, playerHealth + heal)
+          setplayerHealth(newHealth)
+          setStoryText(`You set up a bedroll by the side of the road and rest for a while. You feel refreshed and have ${newHealth} health`)
+        }
+      }
+      function humanityCheck() {
+        setStoryText('You cut yourself and bleed, you are relieved to find that you are still human. A hurting human now, but still human.')
+        setplayerHealth(playerHealth - 1)
       }
       return (
         <main>
@@ -42,8 +57,8 @@ import { useState } from 'react'
           <section>
             <h2>Choices</h2>
             <button onClick={goToCity}>Venture into the city</button>
-              
             <button onClick={checkBackpack}>Check your backpack</button>
+            <button onClick={humanityCheck}>Check if you're still human</button>
             <button onClick={roadsideRest}>Rest by the road</button>
           </section>
         </main>
