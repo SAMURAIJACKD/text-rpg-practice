44ae3b1c81207285f0129f9cb7b08b56e23fc4e1
SAMURAIJACKD <106445386+SAMURAIJACKD@users.noreply.github.com>
2026-06-14 02:44:23 -0500

    feat: add game over message

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,53 @@ import './App.css'
       const playerBackpack = ['flashlight', 'rope', 'prybar']
       const [location, setLocation] = useState('Home')
       const [storyText, setStoryText] = useState(`You open your eyes on a couch, looking around you see your home. You don't remember how you got back home, but as you look at your table you see a note. It reads: "${playerName}, you need to complete the demo, then you can join us in the real game. We'll be waiting for you. - The Devs"`)
-      function goToCity() {
+      function goToCity() {if (playerHealth >= 1) {
         setLocation('City')
         setStoryText('You head into the city to see what you can find. As you walk to the Boulevard you see people bustling about. There are shops, some groups calling out to passersby, and vehicles carrying their passengers. What do you want to do?')
       }
-      function checkBackpack() {
-        setStoryText(`You check your backpack, inside you have: ${playerBackpack.join(', ')}`)
+      else {
+        setStoryText('You died, zombies don\'t exist in this game, maybe wait for a mad scientist to bring you back to life before trying to continue playing?')
       }
+    }
+      function checkBackpack() {if (playerHealth >= 1) {
+        setStoryText(`You check your backpack, inside you have: ${playerBackpack.join(', ')}`)
+      } else {
+        setStoryText('You died, maybe wait for a mad scientist to bring you back to life before trying to continue playing?')
+      }
+        }
       function roadsideRest() {
-        if (playerHealth >= playerMaxHealth) {
-          setStoryText('You consider resting, but realize that you aren\'t actually tired. You decide to move on instead. \(Your health is already full, don\'t be silly\).')
+        if (playerHealth >= 1) {
+          if (playerHealth >= playerMaxHealth) {
+            setStoryText('You consider resting, but realize that you aren\'t actually tired. You decide to move on instead. \(Your health is already full, don\'t be silly\).')
+            return
+          }
+          else {
+            const heal = Math.round(Math.random() * (playerMaxHealth - playerHealth))
+            const newHealth = Math.min(playerMaxHealth, playerHealth + heal)
+            setplayerHealth(newHealth)
+            setStoryText(`You set up a bedroll by the side of the road and rest for a while. You feel refreshed and have ${newHealth} health`)
+          }
+        }
+        else {
+          setStoryText('You died, maybe wait for a mad scientist to bring you back to life before trying to continue playing?')
+        }
+      }
+      function humanityCheck() {if (playerHealth >= 1) {
+        setStoryText('You cut yourself and bleed, you are relieved to find that you are still human. A hurting human now, but still human.')
+        const nextHealth= Math.max(0, playerHealth -1)
+        if (nextHealth <=0) {
+          setStoryText('You were human, but now you are dead. Therefore you are now a corpse. Game over.')
+          setplayerHealth(0)
           return
         }
         else {
-          const heal = Math.round(Math.random() * (playerMaxHealth - playerHealth))
-          const newHealth = Math.min(playerMaxHealth, playerHealth + heal)
-          setplayerHealth(newHealth)
-          setStoryText(`You set up a bedroll by the side of the road and rest for a while. You feel refreshed and have ${newHealth} health`)
-        }
-      }
-      function humanityCheck() {
-        setStoryText('You cut yourself and bleed, you are relieved to find that you are still human. A hurting human now, but still human.')
         setplayerHealth(Math.max(0, playerHealth - 1))
-      }
+      }}
+        else {
+          setStoryText('You died, maybe wait for a mad scientist to bring you back to life before trying to continue playing?')
+        }
+    }
+
       return (
         <main>
           <h1>Text RPG Practice</h1>
