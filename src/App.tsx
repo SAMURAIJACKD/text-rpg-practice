470c9b3435c05d6cd71cf97a69ed731014a3350c
SAMURAIJACKD <106445386+SAMURAIJACKD@users.noreply.github.com>
2026-06-19 14:32:25 -0500

    refactor: check humanity logic updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 
 
     function App() {
-      const deathText = 'You died, maybe wait for a mad scientist to bring you back to life before trying to continue playing?'
+      const deathText = 'You are dead, maybe wait for a mad scientist to bring you back to life before trying to continue playing?'
       const playerName = 'Ricky'
       const playerLevel = 1
       const playerMaxHealth = 20
@@ -11,47 +11,47 @@ import './App.css'
       const playerBackpack = ['flashlight', 'rope', 'prybar']
       const [location, setLocation] = useState('Home')
       const [storyText, setStoryText] = useState(`You open your eyes on a couch, looking around you see your home. You don't remember how you got back home, but as you look at your table you see a note. It reads: "${playerName}, you need to complete the demo, then you can join us in the real game. We'll be waiting for you. - The Devs"`)
-      function goToCity() {if (playerHealth >= 1) {
-        setLocation('City')
-        setStoryText('You head into the city to see what you can find. As you walk to the Boulevard you see people bustling about. There are shops, some groups calling out to passersby, and vehicles carrying their passengers. What do you want to do?')
-      }
-      else {
+      function goToCity() {
+        if (playerHealth >= 1) {
+          setLocation('City')
+          setStoryText('You head into the city to see what you can find. As you walk to the Boulevard you see people bustling about. There are shops, some groups calling out to passersby, and vehicles carrying their passengers. What do you want to do?')
+      } else {
         setStoryText(deathText)
       }
     }
-      function checkBackpack() {if (playerHealth >= 1) {
+      function checkBackpack() {
+        if (playerHealth >= 1) {
         setStoryText(`You check your backpack, inside you have: ${playerBackpack.join(', ')}`)
       } else {
         setStoryText(deathText)
       }
-        }
+    }
       function roadsideRest() {
         if (playerHealth >= 1) {
           if (playerHealth >= playerMaxHealth) {
             setStoryText('You consider resting, but realize that you aren\'t actually tired. You decide to move on instead. \(Your health is already full, don\'t be silly\).')
             return
-          }
-          else {
+          } else {
             const heal = Math.round(Math.random() * (playerMaxHealth - playerHealth))
             const newHealth = Math.min(playerMaxHealth, playerHealth + heal)
             setPlayerHealth(newHealth)
             setStoryText(`You set up a bedroll by the side of the road and rest for a while. You feel refreshed and have ${newHealth} health`)
           }
-        }
-        else {
+        } else {
           setStoryText(deathText)
         }
       }
       function humanityCheck() {
-        const nextHealth= Math.max(0, playerHealth -1)
-        if (playerHealth >= 2) {
-          setStoryText('You cut yourself and bleed, you are relieved to find that you are still human. A hurting human now, but still human.')
-          setPlayerHealth(nextHealth)
-        }
-        else if (playerHealth === 1) {
-          setStoryText('You were human, but now you are dead. Therefore you are now a corpse. Game over.')
-          setPlayerHealth(0)
-          return
+        if (playerHealth >= 1) {
+          const nextHealth= Math.max(0, playerHealth -1)
+          if (nextHealth === 0 ) {
+            setStoryText('You were human, but now you are dead. Therefore you are now a corpse. Game over.')
+            setPlayerHealth(0)
+          } else {
+            setStoryText('You cut yourself and bleed, you are relieved to find that you are still human. A hurting human now, but still human.')
+            setPlayerHealth(nextHealth)
+            return
+          } 
         }
         else {
           setStoryText(deathText)
